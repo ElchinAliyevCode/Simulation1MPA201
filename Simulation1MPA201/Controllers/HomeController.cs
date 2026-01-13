e2e41d6ac731a204c3084463d2c54a872b0c884c
ElchinAliyevCode <elchinfa-mpa201@code.edu.az>
2026-01-13 20:47:39 +0400

    Completed all

--- Simulation1MPA201/Controllers/HomeController.cs
+++ Simulation1MPA201/Controllers/HomeController.cs
@@ -1,13 +1,33 @@
 using System.Diagnostics;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Simulation1MPA201.Contexts;
+using Simulation1MPA201.ViewModels.Product;
 
 namespace Simulation1MPA201.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly SimulationDbContext _context;
+
+        public HomeController(SimulationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products=await _context.Products.Select(x=>new ProductGetVM()
+            {
+                Description = x.Description,
+                CategoryName=x.Category.Name,
+                Price = x.Price,
+                Rating = x.Rating,
+                Title = x.Title,
+                ImagePath = x.ImagePath,
+            }).ToListAsync();
+            return View(products);
         }
 
 
